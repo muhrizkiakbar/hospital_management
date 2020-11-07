f6f688b19205e0df2e45a3637a228427d2243853
muhrizkiakbar <muhrizkiakbar@live.com>
2020-11-07 15:18:50 +0800

    Tes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,21 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+require("jquery3");
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
 
+require("plugin/bootstrap/dist/js/bootstrap.bundle.min");
+require("plugin/jquery.scrollbar/jquery.scrollbar.min");
+require("plugin/jquery-scroll-lock/dist/jquery-scrollLock.min");
+require("plugin/js-cookie/js.cookie");
+require("plugin/list.js/dist/list.min");
+require("plugin/bootstrap-datepicker/dist/js/bootstrap-datepicker.min");
+require("plugin/select2/dist/js/select2.min");
+require("plugin/sweetalert2/dist/sweetalert2.min");
+require("plugin/bootstrap-notify/bootstrap-notify.min");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
