43eccc673ab21322dd9be941448d4d76e0492e5d
Nat4Lia <raztajr@gmail.com>
2020-11-15 00:07:58 +0800

    add style bs-stepper and test example

--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -1,4 +1,35 @@
 $(document).on("turbolinks:load", function () {
+  // Custom Client Side Validation
+  // Example starter JavaScript for disabling form submissions if there are invalid fields
+  (function () {
+    "use strict";
+    console.log('load form validation');
+    window.addEventListener(
+      "load",
+      function () {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName("needs-validation");
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function (form) {
+          form.addEventListener(
+            "submit",
+            function (event) {
+              if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+              }
+              form.classList.add("was-validated");
+            },
+            false
+          );
+        });
+      },
+      false
+    );
+  })();
+});
+
+$(document).on("turbolinks:load", function () {
   $('a[data-toggle="collapse"][data-page="department"][role="button"][aria-expanded="false"]').on('click', function(e) { 
     $(this).text(function(i, v) {
       return v === 'Show' ? 'Hide' : 'Show';
@@ -21,4 +52,9 @@ $(document).on("turbolinks:load", function () {
     }
     
   })
+});
+
+$(document).on("turbolinks:load", function() {
+  const stepper = new Stepper(document.querySelector('.bs-stepper'));
+  
 });
