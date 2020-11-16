e1ebbf2e5bf975e6694b99a48c51205842270f08
muhrizkiakbar <muhrizkiakbar@live.com>
2020-11-16 15:46:53 +0800

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,29 @@
 Rails.application.routes.draw do
-  devise_for :users
 
+  devise_for :users
+  devise_scope :user do
+    authenticated :user do
+      root "users#show_profile", as: "show_profile_authed"
+    end
+
+    unauthenticated do
+      root to: "devise/sessions#new"
+    end
+    
+  end
   
   resources :permissions
   resources :roles, except: [:show] do
     resources :role_permissions, only: [:index, :create]
   end
+
   resources :departments
-  resources :users
+  
+  scope "/manage" do
+    resources :users
+    get "/user/show_profile" => "users#show_profile", as: "show_profile"
+    get "/user/edit_profile" => "users#edit_profile", as: "edit_profile"
+    patch "/user/edit_profile" => "users#update_profile", as: "update_profile"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds/user/create_user.rb
+++ db/seeds/user/create_user.rb
@@ -13,24 +13,30 @@ roles.each do |role|
 
   puts "Create User"
   4.times {
-      User.create(
+      gender_downcase = Faker::Gender.binary_type
+      user = User.create(
         role: add_role,
         email: Faker::Internet.email,
-        username: Faker::Name.unique,
+        username: Faker::Name.initials,
         password: "password123",
+        ktp: Faker::Number.leading_zero_number(digits: 12),
         nip: Faker::Number.leading_zero_number(digits: 10),
         first_name: Faker::Name.first_name,
         last_name: Faker::Name.last_name,
         honorific: Faker::Job.field,
         academic_degree: Faker::Job.education_level,
         date_birth: Faker::Date.between(from: '1982-09-23', to: '2000-09-25'),
-        gender: Faker::Gender.binary_type,
+        gender: gender_downcase.downcase,
         postal_code:  Faker::Number.leading_zero_number(digits: 4),
         address: Faker::Address.full_address,
         rt_number: Faker::Number.leading_zero_number(digits: 2),
         rw_number: Faker::Number.leading_zero_number(digits: 2),
-        medical_staff: Faker::Number.between(from: 1, to: 2)
+        medical_staff: Faker::Number.between(from: 0, to: 1)
       )
+      puts user.medical_staff
+      puts user.gender
+      puts gender_downcase.downcase
+      puts user.errors.full_messages
   }
 end
 
